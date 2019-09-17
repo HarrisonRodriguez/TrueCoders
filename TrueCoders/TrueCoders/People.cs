1ce239de1eb71d3ff2c433cae78b0f631dfd66fc
HarrisonRodriguez <54855519+HarrisonRodriguez@users.noreply.github.com>
2019-09-17 13:30:36 -0500

    Add files via upload

---
+++ TrueCoders/TrueCoders/People.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueCoders
+{
+    public class People
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string HairColor { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
---
+++ TrueCoders/TrueCoders/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Tanner = new People();
+            var Micheal = new People();
+            var Timothy = new People();
+            var Charlie = new People();
+
+            Tanner.Age = 20;
+            Tanner.FirstName = "Tanner";
+            Tanner.LastName = "Riddle";
+            Tanner.HairColor = "Brown";
+
+            Micheal.Age = 32;
+            Micheal.FirstName = "Micheal";
+            Micheal.LastName = "Doyle";
+            Micheal.HairColor = "Black";
+
+            Timothy.Age = 19;
+            Timothy.FirstName = "Timothy";
+            Timothy.LastName = "Tidwell";
+            Timothy.HairColor = "Brown";
+
+            Charlie.Age = 25;
+            Charlie.FirstName = "Charlie";
+            Charlie.LastName = "Doyle";
+            Charlie.HairColor = "Black";
+
+            List<People> trueCoders = new List<People>();
+            trueCoders.Add(Timothy);
+            trueCoders.Add(Micheal);
+            trueCoders.Add(Tanner);
+            trueCoders.Add(Charlie);
+
+            foreach (People person in trueCoders)
+            {
+                Console.WriteLine($"Name:{person.FirstName} {person.LastName}, Age:{person.Age}, Hair Color:{person.HairColor}");
+            }
+        }
+    }
+}
